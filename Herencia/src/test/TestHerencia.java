8e3f0695f1e3428411351363e1862c232b65a221
LuisPerdomo123 <leperdomoo@correo.udistrital.edu.co>
2023-01-30 18:49:14 -0500

    Herencia parte II

---
+++ Herencia/src/test/TestHerencia.java
@@ -0,0 +1,18 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package test;
+
+import domain.Empleado;
+
+/**
+ *
+ * @author lepo9
+ */
+public class TestHerencia {
+    public static void main(String[] args) {
+        Empleado empleado1 = new Empleado();
+        System.out.println("empleado1 = " + empleado1);
+    }
+}
