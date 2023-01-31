b87156092672822cbcc3d2c46a0e55b9a42f2e90
LuisPerdomo123 <leperdomoo@correo.udistrital.edu.co>
2023-01-31 16:48:07 -0500

    Herencia parte IV

--- Herencia/src/test/TestHerencia.java
+++ Herencia/src/test/TestHerencia.java
@@ -17,7 +17,9 @@ public class TestHerencia {
         Empleado empleado1 = new Empleado("Juan", 5000.0);
         System.out.println("empleado1 = " + empleado1);
         
-        Cliente cliente1 = new Cliente(new Date(), true, "Karla", 'F', 28, "Saturno 15");
+        var fecha = new Date();
+        
+        Cliente cliente1 = new Cliente(fecha, true, "Karla", 'F', 28, "Saturno 15");
         System.out.println("cliente1 = " + cliente1);
     }
 }
