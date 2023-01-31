f4b02acc0e2508db504090964e1de0821b0654f5
LuisPerdomo123 <leperdomoo@correo.udistrital.edu.co>
2023-01-31 15:27:53 -0500

    Herencia parte III

--- Herencia/src/test/TestHerencia.java
+++ Herencia/src/test/TestHerencia.java
@@ -12,7 +12,7 @@ import domain.Empleado;
  */
 public class TestHerencia {
     public static void main(String[] args) {
-        Empleado empleado1 = new Empleado();
+        Empleado empleado1 = new Empleado("Juan", 5000.0);
         System.out.println("empleado1 = " + empleado1);
     }
 }
